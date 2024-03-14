d456839514160d305a6aa85509cb80a90fbb4b9b
Alexandria Vannorsdall <arvannorsdall@yahoo.com>
2024-03-14 14:47:56 +0000

    func: created Order and Item models

---
+++ app/Models/Item.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Item extends Model
+{
+    use HasFactory;
+
+    /**
+     * @var string[]
+     */
+    protected $fillable = [
+        'order_id',
+        'sku',
+        'qty',
+        'cost',
+    ];
+
+    /**
+     * @return BelongsTo
+     */
+    public function order(): BelongsTo
+    {
+        return $this->belongsTo(Order::class);
+    }
+}
+
